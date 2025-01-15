7e69688001aad0846461eba639e2bccc26e55aa2
Minji Son <109676875+midday2612@users.noreply.github.com>
2025-01-15 20:42:34 +0900

    [Silver V] Title: 덩치, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,16 +1,14 @@
 n = int(input())
-max_x = 9
-max_y = 9
-arr = []
+hwList = []
+countList = []
+for i in range(n):
+    hw = list(map(int, input().split()))
+    hwList.append(hw)
 
 for i in range(n):
-    x, y = map(int, input().split())
-    arr.append([x, y, 1])
-
-for i in range(n):
+    tmpCount = 1
     for j in range(n):
-        if (arr[i][0]<arr[j][0] and arr[i][1]<arr[j][1]):
-            arr[i][2] += 1
-
-for i in range(n):
-    print(arr[i][2], end = ' ')
+        if (hwList[i][0] < hwList[j][0]) and (hwList[i][1] < hwList[j][1]):
+            tmpCount += 1
+    countList.append(tmpCount)
+print(*countList)
